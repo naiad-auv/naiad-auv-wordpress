0c74d03429bed5087be947f0feadd6b0871e4209
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-09-17 14:06:51 +0200

    Set up basic structure for configuration files.

---
+++ wp-config.local-sample.php
@@ -0,0 +1,27 @@
+<?php
+//
+// ### Installation
+// Copy this file to "wp-config.local.php" and change the values.
+//
+// MySQL settings - You can get this info from your web host
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+// Authentication Unique Keys and Salts.
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+// Other variables
+$table_prefix  = 'wp_';
+define('WPLANG', '');
+define('WP_DEBUG', false);
