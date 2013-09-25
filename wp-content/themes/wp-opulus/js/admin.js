2e3833a7cfdffbdd3edc94b907615d93596e6019
naiad <naiad@mothership.(none)>
2013-09-25 10:07:31 +0200

    Removed wp-opulus theme due to difficulties with configuration of blog page.

--- wp-content/themes/wp-opulus/js/admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-
-    jQuery("#seo_description").charCount({
-        allowed: 160 ,		
-        warning: 20,
-        counterText: 'Recomended Characters Left: '	
-    });
-    
-});
