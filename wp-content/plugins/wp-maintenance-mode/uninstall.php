daa4272f487e20c7ae82cb78124b9608ed1ee745
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-09-25 11:03:59 +0200

    Removed wp-mainenance-mode plugin so I can add it as submodule

--- wp-content/plugins/wp-maintenance-mode/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit();
-
-delete_site_option( 'wp-maintenance-mode' );
-delete_site_option( 'wp-maintenance-mode-msqld' );
-delete_option( 'wp-maintenance-mode' );
-delete_option( 'wp-maintenance-mode-msqld' );
